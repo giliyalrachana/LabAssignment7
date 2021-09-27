9924634bfc115961c64ff4ab6f11c59e0d028357
Rachana <giliyalrachana@gmail.com>
2021-09-27 22:29:45 +0530

    New classes are added

---
+++ LabAssignment7/LabAssignment7/ConstructorDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace LabAssignment7
+{
+    class ConstructorDemo
+    {
+        static void Main()
+        {
+            Room room1 = new Room("number", "floor", "type", "capacity", "bookedTime", "price");
+            Room room2 = new Room(room1);
+            Console.WriteLine($"Number={ room2.Number}\n Floor={ room2.Floor}\n Type= {room2.Type}\n Capacity={ room2.Capacity}\n BookedTime= { room2.BookedTime }\n Price= { room2.Price }" );
+            Console.ReadLine();
+        }
+            
+    }
+}
---
+++ LabAssignment7/LabAssignment7/Room.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace LabAssignment7
+{
+    class Room
+    {
+        public int Number { get; set; }
+        public int Floor { get; set; }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public DateTime BookedTime { get; set; }
+        public double Price { get; set; }
+        public Room()
+        {
+            Console.WriteLine("Default constructor of Room");
+        }
+        public Room(int number, int floor, string type, int capacity, DateTime bookedTime, double price)
+        {
+            this.Number = number;
+            this.Floor = floor;
+            this.Type = type;
+            this.Capacity = capacity;
+            this.BookedTime = bookedTime;
+            this.Price = price;
+        }
+        // Copy constructor
+        public Room(Room room)
+        {
+            this.Number = room.Number;
+            this.Floor = room.Floor;
+            this.Type = room.Type;
+            this.Capacity = room.Capacity;
+            this.BookedTime = room.BookedTime;
+            this.Price = room.Price;
+
+        }
+        
+
+
+
+    }
+}
