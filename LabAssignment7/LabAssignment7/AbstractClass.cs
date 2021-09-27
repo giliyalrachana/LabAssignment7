cf4c6a509f248bf5d4f270a1c664f2b105f0a043
Rachana <giliyalrachana@gmail.com>
2021-09-27 21:25:56 +0530

    New classes are added

---
+++ LabAssignment7/LabAssignment7/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace LabAssignment7
+{
+    // This program demo. how to use abstract class
+    class AbstractClass
+    {
+        static void Main()
+        {
+            Laptop mac = new Laptop(122,"hp","1995");
+            Console.WriteLine(mac.AboutAbstractComputer());
+            Console.WriteLine(mac.BootUp());
+            Console.WriteLine(mac.ShutDown());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ LabAssignment7/LabAssignment7/AbstractComputer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LabAssignment7
+{
+    public abstract class AbstractComputer
+    {
+        public int ModelNo { get; set; }
+        public string ModelName { get; set; }
+        public string Year { get; set; }
+        public AbstractComputer(int modelNo, string modelName, string year)
+        {
+            this.ModelNo = modelNo;
+            this.ModelName = modelName;
+            this.Year = year;
+        }
+        public string AboutAbstractComputer()
+        {
+            return $"Model No={this.ModelNo}\nName={this.ModelName}\nYear={this.Year}";
+        }
+        public string BootUp()
+        {
+            return "BootUp() calling from AbstractComputer";
+        }
+        public string ShutDown()
+        {
+            return "ShutDown() calling from AbstractComputer";
+        }
+    }
+    class Laptop : AbstractComputer
+    {
+        public Laptop(int modelNo,string modelName, string year) : base(modelNo,modelName, year)
+        {
+
+        }
+        public string Letters(int noOfLetters)
+        {
+            return $"This is {noOfLetters} Laptop";
+        }
+    }
+  
+
+    
+}
